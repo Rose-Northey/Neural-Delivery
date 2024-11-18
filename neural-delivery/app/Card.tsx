0363c4e2410b18193e8d579621ba3dea037d4702
rose-northey <rose.northey@octopus.com>
2024-11-18 14:04:33 +1300

    WIP

--- neural-delivery/app/Card.tsx
+++ neural-delivery/app/Card.tsx
@@ -1,6 +1,11 @@
 import { Component } from "react";
 
 export default class Card extends Component {
+  moveCount: number;
+  constructor(props: number) {
+    super(props);
+    this.moveCount = 3;
+  }
   render() {
     return (
       <>
