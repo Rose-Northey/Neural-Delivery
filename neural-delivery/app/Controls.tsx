839152cfb09b0ab08caf578d2def031d183ec2c0
Rose Northey <rose.northey@octopus.com>
2024-12-04 10:17:12 +1300

    refactor functions

--- neural-delivery/app/Controls.tsx
+++ neural-delivery/app/Controls.tsx
@@ -2,42 +2,44 @@ import { css } from "@emotion/css";
 import { Component } from "react";
 
 type ControlsProps = {
-  onResetGameClick: () => void;
-  moveCount: number;
+    onResetGameClick: () => void;
+    moveCount: number;
 };
 export default class Controls extends Component<ControlsProps> {
-  constructor(props: ControlsProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className={style.controlsContainer}>
-        <div className={style.moveCountContainer}>
-          MoveCount:
-          <div className={style.countContainer}>{this.props.moveCount}</div>
-        </div>
-        <button onClick={this.props.onResetGameClick}>New Game</button>
-      </div>
-    );
-  }
+    constructor(props: ControlsProps) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className={style.controlsContainer}>
+                <div className={style.moveCountContainer}>
+                    MoveCount:
+                    <div className={style.countContainer}>
+                        {this.props.moveCount}
+                    </div>
+                </div>
+                <button onClick={this.props.onResetGameClick}>New Game</button>
+            </div>
+        );
+    }
 }
 
 const style = {
-  controlsContainer: css({
-    display: "flex",
-    flexDirection: "column",
-    backgroundColor: "#B9D9EB",
-    padding: "1rem",
-    gap: "1rem",
-    height: "100%",
-  }),
-  countContainer: css({
-    fontSize: "2rem",
-    color: "#008E97",
-  }),
-  moveCountContainer: css({
-    display: "flex",
-    alignItems: "center",
-    gap: "1rem",
-  }),
+    controlsContainer: css({
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "#B9D9EB",
+        padding: "1rem",
+        gap: "1rem",
+        height: "100%",
+    }),
+    countContainer: css({
+        fontSize: "2rem",
+        color: "#008E97",
+    }),
+    moveCountContainer: css({
+        display: "flex",
+        alignItems: "center",
+        gap: "1rem",
+    }),
 };
--- neural-delivery/app/Game.tsx
+++ neural-delivery/app/Game.tsx
@@ -8,173 +8,175 @@ import Card from "./Card";
 type GameProps = {};
 
 export type CardData = {
-  id: number;
-  image: string;
-  randomization: number;
-  isMatched: boolean;
-  isSelected: boolean;
+    id: number;
+    image: string;
+    randomization: number;
+    isMatched: boolean;
+    isSelected: boolean;
 };
 
 type GameState = {
-  moveCount: number;
-  cards: CardData[];
+    moveCount: number;
+    cards: CardData[];
 };
 
 export default class Game extends Component<GameProps, GameState> {
-  images: string[];
-  constructor(props: GameProps) {
-    super(props);
-    this.images = [
-      "/images/blackCat.jpg",
-      "/images/horse.jpg",
-      "/images/box.jpg",
-      "/images/uke.jpg",
-      "/images/plant.jpg",
-      "/images/duck.jpg",
-    ];
-    this.state = {
-      moveCount: 0,
-      cards: this.generateCards(this.images),
-    };
-    this.handleResetGameClick = this.handleResetGameClick.bind(this);
-    this.handleUnknownCardClick = this.handleUnknownCardClick.bind(this);
-  }
-
-  handleResetGameClick = () => {
-    this.setState({ moveCount: 0, cards: this.generateCards(this.images) });
-  };
-
-  unselectAllSelectedCards = () => {
-    this.setState((prevState) => {
-      const cardsAfterNoPairFound = prevState.cards.map((card) => {
-        if (card.isSelected === true) {
-          return { ...card, isSelected: false };
-        }
-        return card;
-      });
-      return {
-        moveCount: prevState.moveCount + 1,
-        cards: cardsAfterNoPairFound,
-      };
-    });
-  };
-
-  generateCards = (cardImages: string[]): CardData[] => {
-    const newStack: CardData[] = [];
-    cardImages.forEach((image) => {
-      const cardData1 = {
-        image: image,
-        id: newStack.length,
-        randomization: Math.random(),
-        isMatched: false,
-        isSelected: false,
-      };
-      const cardData2 = {
-        image: image,
-        id: newStack.length + 1,
-        randomization: Math.random(),
-        isMatched: false,
-        isSelected: false,
-      };
-      newStack.push(cardData1, cardData2);
-    });
-    return shuffleCards(newStack);
-  };
-
-  handleUnknownCardClick = (cardId: number, image: string) => {
-    const selectedCards = this.state.cards.filter(
-      (card) => card.isSelected === true
-    );
-    this.revealCard(cardId);
-    if (selectedCards.length === 1) {
-      const firstCardImage = selectedCards[0].image;
-      this.handleSecondUnknownCardClick(firstCardImage, image);
+    images: string[];
+    constructor(props: GameProps) {
+        super(props);
+        this.images = [
+            "/images/blackCat.jpg",
+            "/images/horse.jpg",
+            "/images/box.jpg",
+            "/images/uke.jpg",
+            "/images/plant.jpg",
+            "/images/duck.jpg",
+        ];
+        this.state = {
+            moveCount: 0,
+            cards: this.generateCards(this.images),
+        };
+        this.handleResetGameClick = this.handleResetGameClick.bind(this);
+        this.handleUnknownCardClick = this.handleUnknownCardClick.bind(this);
     }
-  };
 
-  handleSecondUnknownCardClick = (
-    firstCardImage: string,
-    secondCardImage: string
-  ) => {
-    if (firstCardImage === secondCardImage) {
-      // wait one second
-      setTimeout(
-        () =>
-          this.setState((prevState) => {
-            const cardsAfterPairFound = prevState.cards.map((card) => {
-              if (card.image === firstCardImage) {
-                return { ...card, isMatched: true, isSelected: false };
-              }
-              return card;
+    handleResetGameClick = () => {
+        this.setState({ moveCount: 0, cards: this.generateCards(this.images) });
+    };
+
+    generateCards = (cardImages: string[]): CardData[] => {
+        const newStack: CardData[] = [];
+        cardImages.forEach((image) => {
+            const cardData1 = {
+                image: image,
+                id: newStack.length,
+                randomization: Math.random(),
+                isMatched: false,
+                isSelected: false,
+            };
+            const cardData2 = {
+                image: image,
+                id: newStack.length + 1,
+                randomization: Math.random(),
+                isMatched: false,
+                isSelected: false,
+            };
+            newStack.push(cardData1, cardData2);
+        });
+        return shuffleCards(newStack);
+    };
+
+    handleUnknownCardClick = (
+        currentCardId: number,
+        currentCardImage: string
+    ) => {
+        const previousCard = this.state.cards.filter(
+            (card) => card.isSelected === true
+        )[0];
+        this.markCurrentCardAsSelected(currentCardId);
+        if (previousCard) {
+            this.increaseMoveCount();
+            setTimeout(() => {
+                if (previousCard.image === currentCardImage) {
+                    this.markPreviousAndCurrentCardsAsMatched(
+                        previousCard.image
+                    );
+                } else {
+                    this.unselectAllCards();
+                }
+            }, 1000);
+        }
+    };
+
+    markCurrentCardAsSelected = (cardId: number) => {
+        this.setState((prevState) => {
+            const updatedCards = prevState.cards.map((card) => {
+                if (card.id === cardId) {
+                    return { ...card, isSelected: true };
+                }
+                return card;
+            });
+            return { ...prevState, cards: updatedCards };
+        });
+    };
+    unselectAllCards = () => {
+        this.setState((prevState) => {
+            const updatedCards = prevState.cards.map((card) => {
+                if (card.isSelected === true) {
+                    return { ...card, isSelected: false };
+                }
+                return card;
             });
             return {
-              moveCount: prevState.moveCount + 1,
-              cards: cardsAfterPairFound,
+                moveCount: prevState.moveCount + 1,
+                cards: updatedCards,
             };
-          }),
-        1000
-      );
-    } else {
-      this.unselectAllSelectedCards();
+        });
+    };
+    markPreviousAndCurrentCardsAsMatched = (matchedImage: string) => {
+        this.setState((prevState) => {
+            const updatedCards = prevState.cards.map((card) => {
+                if (card.image === matchedImage) {
+                    return { ...card, isMatched: true, isSelected: false };
+                }
+                return card;
+            });
+            return {
+                moveCount: prevState.moveCount + 1,
+                cards: updatedCards,
+            };
+        });
+    };
+
+    increaseMoveCount = () => {
+        this.setState((prevState) => {
+            return { ...prevState, moveCount: prevState.moveCount + 1 };
+        });
+    };
+
+    render() {
+        return (
+            <div className={styles.gridAndControlsContainer}>
+                <div className={styles.grid}>
+                    {this.state.cards.map((cardData) => {
+                        return (
+                            <Card
+                                key={cardData.id}
+                                onUnknownCardClick={this.handleUnknownCardClick}
+                                id={cardData.id}
+                                image={cardData.image}
+                                isSelected={cardData.isSelected}
+                                isMatched={cardData.isMatched}
+                            />
+                        );
+                    })}
+                </div>
+                <Controls
+                    onResetGameClick={this.handleResetGameClick}
+                    moveCount={this.state.moveCount}
+                />
+            </div>
+        );
     }
-  };
-
-  revealCard = (cardId: number) => {
-    this.setState((prevState) => {
-      const cardsAfterCardClick = prevState.cards.map((card) => {
-        if (card.id === cardId) {
-          return { ...card, isSelected: true };
-        }
-        return card;
-      });
-      console.log("mid reveal card hit");
-      return { ...prevState, cards: cardsAfterCardClick };
-    });
-  };
-
-  render() {
-    return (
-      <div className={styles.gridAndControlsContainer}>
-        <div className={styles.grid}>
-          {this.state.cards.map((cardData) => {
-            return (
-              <Card
-                key={cardData.id}
-                onUnknownCardClick={this.handleUnknownCardClick}
-                id={cardData.id}
-                image={cardData.image}
-                isSelected={cardData.isSelected}
-                isMatched={cardData.isMatched}
-              />
-            );
-          })}
-        </div>
-        <Controls
-          onResetGameClick={this.handleResetGameClick}
-          moveCount={this.state.moveCount}
-        />
-      </div>
-    );
-  }
 }
 
 const styles = {
-  gridAndControlsContainer: css({
-    minHeight: "90%",
-    backgroundColor: "#0066b2",
-    display: "flex",
-    width: "100%",
-    padding: "1rem",
-    justifyContent: "center",
-    alignItems: "center",
-  }),
-  grid: css({
-    display: "flex",
-    flexWrap: "wrap",
-    maxWidth: "40rem",
-    gap: "0.5rem",
-    "& img": {
-      width: "150px",
-    },
-  }),
+    gridAndControlsContainer: css({
+        minHeight: "90%",
+        backgroundColor: "#0066b2",
+        display: "flex",
+        width: "100%",
+        padding: "1rem",
+        justifyContent: "center",
+        alignItems: "center",
+    }),
+    grid: css({
+        display: "flex",
+        flexWrap: "wrap",
+        maxWidth: "40rem",
+        gap: "0.5rem",
+        "& img": {
+            width: "150px",
+        },
+    }),
 };
