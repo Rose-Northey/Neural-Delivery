8aa81e71dea3e62c057148fcd69836de241c6b57
Rose Northey <rose.northey@octopus.com>
2024-12-04 15:59:51 +1300

    card flip working but styling strange

--- neural-delivery/app/Card.tsx
+++ neural-delivery/app/Card.tsx
@@ -1,27 +1,95 @@
-import { css } from "@emotion/css";
+import { css, cx } from "@emotion/css";
 import { Component } from "react";
 
 interface NewCardProps {
-  image: string;
-  id: number;
-  onUnknownCardClick: (cardId: number, image: string) => void;
-  isSelected: boolean;
-  isMatched: boolean;
+    image: string;
+    id: number;
+    onUnknownCardClick: (cardId: number, image: string) => void;
+    isSelected: boolean;
+    isMatched: boolean;
 }
 
 export default class Card extends Component<NewCardProps> {
-  constructor(props: NewCardProps) {
-    super(props);
-    this.state = {
-      isMatched: false,
-      isSelected: false,
-    };
-  }
+    constructor(props: NewCardProps) {
+        super(props);
+        this.state = {
+            isMatched: false,
+            isSelected: false,
+        };
+    }
 
-  render() {
-    return (
-      <>
-        {this.props.isSelected || this.props.isMatched ? (
+    render() {
+        return (
+            <>
+                {this.props.isMatched ? (
+                    <img key={this.props.id} src={this.props.image} />
+                ) : (
+                    <div className={style.card}>
+                        <div
+                            className={cx(
+                                style.cardInner,
+                                this.props.isSelected ? style.isFlipped : ""
+                            )}
+                        >
+                            <img
+                                onClick={() =>
+                                    this.props.onUnknownCardClick(
+                                        this.props.id,
+                                        this.props.image
+                                    )
+                                }
+                                src="/images/unknown.jpg"
+                                className={style.isUnknown}
+                            />
+                            <img
+                                key={this.props.id}
+                                src={this.props.image}
+                                className={style.isSelected}
+                            />
+                        </div>
+                    </div>
+                )}
+            </>
+        );
+    }
+}
+
+const style = {
+    isFlipped: css({
+        transform: "rotateY(180deg)",
+    }),
+    card: css({
+        width: "150px",
+        height: "150px",
+        perspective: "1000px",
+    }),
+    cardInner: css({
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        transition: "transform 0.8s",
+        transformStyle: "preserve-3d",
+        "&>img": {
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+        },
+    }),
+
+    isUnknown: css({
+        zIndex: "2",
+        "&:hover": { boxShadow: "0 0 0 2px #7d90a1" },
+        backfaceVisibility: "hidden",
+    }),
+    isSelected: css({
+        opacity: "75%",
+        boxShadow: "0 0 0 2px #FEBE10",
+    }),
+};
+
+// cards have an imageSource and this also acts as a unique ID
+{
+    /* {this.props.isSelected || this.props.isMatched ? (
           <img
             key={this.props.id}
             src={this.props.image}
@@ -35,20 +103,5 @@ export default class Card extends Component<NewCardProps> {
             className={style.isUnknown}
             src="/images/unknown.jpg"
           />
-        )}
-      </>
-    );
-  }
+        )} */
 }
-
-const style = {
-  isSelected: css({
-    opacity: "75%",
-    boxShadow: "0 0 0 2px #FEBE10",
-  }),
-  isUnknown: css({
-    "&:hover": { boxShadow: "0 0 0 2px #7d90a1" },
-  }),
-};
-
-// cards have an imageSource and this also acts as a unique ID
