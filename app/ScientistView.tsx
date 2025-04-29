197c49a1915b670c779a42b8ccf217fe6383e397
Rose Northey <rose.northey@octopus.com>
2025-04-30 10:25:17 +1200

    appearance changes to scientist view

--- app/ScientistView.tsx
+++ app/ScientistView.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import Game from "./Game";
 import { CardData, GameState } from "./models";
 import { allImagesAndIds } from "./allImagesAndIds";
+import { colors } from "./colors";
 
 export default function ScientistView() {
     const { data, error, isLoading } = useQuery({
@@ -33,16 +34,17 @@ export default function ScientistView() {
     }
 
     return (
-        <div className={previousGameButtons}>
+        <div className={gameGridContainer}>
+        <div className={gameGrid}>
             {data?.map((game) => {
                 const cards = generateCardData(game.pictureIdLayout);
                 return (
-                    <div key={game.id}>
-                        <div>{`game${game.id}`}</div>
+                    <div className={gameCard(game.difficulty)} key={game.id}>
+                        <div>{`Game ${game.id}`}</div>
                         <div>{`difficulty: ${game.difficulty}`}</div>
                         <div>{`total moves:${game.userMoves.length / 2}`}</div>
                         <button onClick={handleViewThisGameClick}>
-                            Replay This Game
+                            replay this game
                         </button>
                         <Game
                             initialCards={cards}
@@ -53,24 +55,53 @@ export default function ScientistView() {
                 );
             })}
         </div>
+        </div>
     );
 }
 
-const previousGameButtons = css({
+const gameGridContainer=css({
+    display:"flex",
+    justifyContent:"center",
+    width:"100%"
+   })
+
+const gameGrid = css({
     display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    flexWrap: "wrap",
+    justifyContent:"space-between",
     gap: "1rem",
     margin: "1rem",
-    "&& > div": {
+    width: "70%",
+});
+
+const gameCard = (difficulty: string)=>css({
+    "&&": {
         border: "solid green 1pt",
         padding: "0.5rem",
+        width: difficulty === "easiest"? "10rem":"20rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignSelf: "center",
+        alignItems:"center"
+
     },
     "&& > button": {
+        margin: "0.5rem",
+        maxWidth: "10rem",
+        backgroundColor: colors.lightBlue,
+        borderColor:colors.darkBlue,
         padding: "0.5rem",
-        maxWidth: "5rem",
     },
-});
+    "&&&> button:hover":{
+        backgroundColor: colors.orange,
+    },
+    "&&&> button:active":{
+        borderColor: colors.orange,
+    }
+
+})
+
 
 /*when you click view this game you see a replay pop up just underneth
 view the game box. 
